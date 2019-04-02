2a4448567c2ec3127ed77d5dbca0e99b7d781b05
eveningkid <arn.dellinger@gmail.com>
2019-04-02 06:10:06 +0200

    Fix k-means implementation (random state)

--- src/cluster/k-means.js
+++ src/cluster/k-means.js
@@ -1,34 +1,21 @@
 const MLKMeans = require('ml-kmeans');
 
 class KMeans {
-  constructor({ nClusters, randomState }) {
+  constructor({ nClusters, randomState = Math.ceil(Math.random() * 999) }) {
     this.model = null;
     this.nClusters = nClusters;
-    this.randomState = randomState || Math.ceil(Math.random() * 999);
+    this.randomState = randomState;
     this.clusterCenters_ = [];
   }
 
   // y is ignored
   fit(X, y = null, sample_weight) {
-    const centers = this._generateRandomCenters(this.nClusters, X);
     this.model = MLKMeans(X, this.nClusters, {
-      initialization: centers,
       seed: this.randomState,
     });
     this.clusterCenters_ = this.model.centroids.map(({ centroid }) => centroid);
     this.labels_ = this.model.clusters;
     return this;
-  }
-
-  _generateRandomCenters(nClusters, X) {
-    const XCopy = [...X];
-    const centroids = Array(nClusters).fill(0);
-    return centroids.map(() => {
-      const randomId = Math.floor(Math.random() * XCopy.length);
-      const centroid = XCopy[randomId];
-      XCopy.splice(randomId, 1);
-      return centroid;
-    });
   }
 
   fitPredict(X, y, sample_weight) {
